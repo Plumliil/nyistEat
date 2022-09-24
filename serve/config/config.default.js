f4b5ab0780d91a927ce2415d33f2d71367f7d0df
plumliil <476453574@qq.com>
2022-09-24 23:36:19 +0800

    serve config

--- serve/config/config.default.js
+++ serve/config/config.default.js
@@ -1,10 +1,10 @@
 // // 默认配置
 // module.exports={
 //     dbUri:'mongodb://root:123456@124.70.8.61:27017/blogData?authSource=admin',
-//     jwtSecret:'d05965ae-0b9b-11ec-9a03-0242ac130003'
 // }
 
 // 默认配置
-module.exports={
-    dbUri:'mongodb://localhost:27017/nyistEat'
+module.exports = {
+    dbUri: 'mongodb://localhost:27017/nyistEat',
+    jwtSecret: 'd05965ae-0b9b-11ec-9a03-0242ac130003'
 }
