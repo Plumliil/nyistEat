4fa8855aaf28c0e750a01ef31704aacd2dea1aa2
plumliil <476453574@qq.com>
2022-09-24 23:35:33 +0800

    serve model user

--- serve/model/user.js
+++ serve/model/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const baseModel = require('./base_model');
-const md5 = require('../util/md5')
-const {
-    set
-} = require("mongoose");
+// const md5 = require('../util/md5')
+// const {
+//     set
+// } = require("mongoose");
 const userSchema = new mongoose.Schema({
     ...baseModel,
-    username: {
+    name: {
         type: String,
         required: true
     },
@@ -25,8 +25,8 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        set: value => md5(value),
-        select: false
+        // set: value => md5(value),
+        // select: false
     },
     image: {
         type: String,
@@ -34,10 +34,12 @@ const userSchema = new mongoose.Schema({
     },
     collect: {
         type: [Object],
+        default:[],
         require: false
     },
     like: {
         type: [Object],
+        default:[],
         require: false
     }
 })
