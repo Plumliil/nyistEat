ab0c946c0ef53856a30bf55df65af24bf033a011
plumliil <476453574@qq.com>
2022-09-25 23:54:25 +0800

    frontdesk utils

--- frontdesk/util/utils.js
+++ frontdesk/util/utils.js
@@ -30,7 +30,8 @@ function transAddress(addressArray) {
 	})
 	return res
 }
-function cache(key, value, time = 3600*3600*3600*3600) {
+
+function cache(key, value, time = 3600 * 3600 * 3600 * 3600) {
 	let nowTime = Date.parse(new Date()) / 1000;
 	if (key && value) {
 		// 设置到期时间
@@ -53,7 +54,30 @@ function cache(key, value, time = 3600*3600*3600*3600) {
 		}
 	}
 }
+
+// 格式化日期 年月日 
+function timeFormat(type) {
+
+	let date = new Date();
+	let year = date.getFullYear();
+	let month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
+	let day = date.getDay() > 9 ? date.getDay() : '0' + date.getDay();
+	let dates = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
+	let houre = date.getHours() > 9 ? date.getHours() : '0' + date.getHours();
+	let min = date.getMinutes() > 9 ? date.getMinutes() : '0' + date.getMinutes();
+	let nowTime = '';
+	if (type === 'YY-MM-DD hh:mm') {
+		nowTime = year + '-' + month + '-' + dates + '   ' + houre + '-' + min;
+		return nowTime
+	} else if (type === 'MM-DD hh-mm') {
+		nowTime = month + '-' + dates + '   ' + houre + ':' + min;
+		return nowTime
+	}
+
+}
+timeFormat()
 module.exports = {
 	transAddress,
-	cache
+	cache,
+	timeFormat
 }
