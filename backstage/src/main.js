ed4a6c9a404bbcdd105fa834360dd62328ec61c8
plumliil <476453574@qq.com>
2022-10-13 21:19:16 +0800

    Application profiles

--- backstage/src/main.js
+++ backstage/src/main.js
@@ -8,6 +8,7 @@ import router from './router'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3366/api/'
+// axios.defaults.baseURL = 'http://180.76.195.252:3366/api/'
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 const app = createApp(App)
--- serve/config/config.default.js
+++ serve/config/config.default.js
@@ -5,7 +5,7 @@
 
 // 默认配置
 module.exports = {
-    dbUri: 'mongodb://localhost:27017/nyistEat',
-    // dbUri: 'mongodb://rootLi:123456@180.76.195.252:27017/NyistEat?authSource=admin',
+    // dbUri: 'mongodb://localhost:27017/nyistEat',
+    dbUri: 'mongodb://rootLi:123456@180.76.195.252:27017/NyistEat?authSource=admin',
     jwtSecret: 'd05965ae-0b9b-11ec-9a03-0242ac130003'
 }
